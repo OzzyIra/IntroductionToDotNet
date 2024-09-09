bbcd64fa14fabc00ae76f0cc18ab9f9ded435c57
OzzyIra <https://github.com/OzzyIra/IntroductionToDotNet.git>
2024-09-09 15:14:15 +0300

    Factorial in progress

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace Factorial
 {
+    
     class Program
     {
+   
+        static int CalculateFactorial(int number)
+        {
+            if (number == 0 || number == 1) return 1;
+           
+             int sum = 1;
+                for (int i = 1; i <= number; i++)
+                {
+
+                    sum *= i;
+                }
+                return sum;     
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите ваше число");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalculateFactorial(number));
+           
         }
+
     }
+        
 }
